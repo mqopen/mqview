81bbbac5b77b2ddfcda1ab642f99f4da64770107
Ivo Slanina <ivo.slanina@gmail.com>
2016-02-12 14:47:50 +0100

    Added links to tree panel.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     guardData: Ember.inject.service('guard-data'),
+    treeData: null,
     init: function() {
         this._super();
         var socket = this.get('websockets').socketFor('ws://localhost:8765/');
@@ -10,23 +11,9 @@ export default Ember.Controller.extend({
         socket.on('close', function(event) {
             console.log('closed');
         }, this);
-        this.get('guardData').addDeviceObserver(this, function(sender, key, value, rev) {
-            var tData = this.get('treeData');
-            var devices = this.get('guardData').getDevices();;
-            if (tData == null) {
-                var deviceList = []
-                for (var i = 0; i < devices.length; i++) {
-                    var device = devices[i];
-                    deviceList.push({text: device.name});
-                }
-                tData = {
-                    text: "devices",
-                    children: deviceList};
-            }
-            this.set('treeData', tData);
-        });
+        this.get('guardData').addDeviceObserver(this, 'updateTree');
+        this.set('treeData', this.getTreeBaseData());
     },
-    treeData: null,
 
     myOpenHandler: function(event) {
         console.log('On open event has been called: ' + event);
@@ -39,5 +26,60 @@ export default Ember.Controller.extend({
             this.get('guardData').initBrokers(msg.brokers);
         } else if (msg.feed == "update") {
         }
+    },
+
+    updateTree: function(sender, key, value, rev) {
+        var tData = this.getTreeBaseData();
+        var devices = this.get('guardData').getDevices();;
+        var deviceList = []
+        for (var i = 0; i < devices.length; i++) {
+            var device = devices[i];
+            tData[1].children.push(
+                {
+                    text: device.name,
+                    a_attr: {
+                        href: "detail/device/" + device.name
+                    },
+                    li_attr: {
+                        class: "device-node"
+                    }
+                });
+        }
+        this.set('treeData', tData);
+        console.log(this.get('treeData'));
+    },
+
+    getTreeBaseData: function() {
+        return [
+            {
+                text: "General",
+                state: {
+                    selected: true
+                },
+                li_attr: {
+                    class: "tree-general"
+                }
+            },
+            {
+                text: "Devices",
+                state: {
+                    opened: true
+                },
+                children: []
+            }
+        ];
+    },
+
+    actions: {
+        handleTreeDidBecomeReady: function() {
+            var treeObject = this.get('jstreeObject');
+            treeObject.on('select_node.jstree', function(e, data) {
+                if (data.node.li_attr.class == "tree-general") {
+                    this.transitionToRoute("general");
+                } else if (data.node.li_attr.class == "device-node") {
+                    this.transitionToRoute("detail.device", data.node.text);
+                }
+            }.bind(this));
+        },
     },
 });
