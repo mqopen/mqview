349d3d94c2150546f7045266a41d67c546b3268e
Ivo Slanina <ivo.slanina@gmail.com>
2016-02-13 10:07:54 +0100

    Fixed renderin model at general view.

--- app/controllers/general.js
+++ app/controllers/general.js
@@ -1,38 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
-    guardData: Ember.inject.service('guard-data'),
-    devices: null,
-
-    init: function() {
-        this._super(...arguments);
-        this.get('guardData').addDeviceObserver(this, 'onGuardDataUpdate');
-    },
-
-    onGuardDataUpdate: function(sender, key, value, rev) {
-        var devices = this.get('guardData').getDevices();
-        var _d = [];
-        for (var i = 0; i < devices.length; i++) {
-            var device = devices[i];
-            var status = "device-ok";
-            if (device.status != "ok")
-                status = "device-error"
-            var guardCount = 0;
-            var alarmCount = 0;
-            for (var j = 0; j < device.guards.length; j++) {
-                var guard = device.guards[j];
-                guardCount++;
-                alarmCount += guard.alarms.length;
-            }
-            _d.push(
-                {
-                    name: device.name,
-                    guardCount: guardCount,
-                    alarmCount: alarmCount,
-                    status: status,
-                });
-        }
-        this.set('devices', _d);
-    },
 });
--- app/routes/general.js
+++ app/routes/general.js
@@ -1,4 +1,42 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    guardData: Ember.inject.service('guard-data'),
+
+    init: function() {
+        this._super(...arguments);
+        this.get('guardData').addDeviceObserver(this, function(sender, key, value, rev) {
+            this.refresh();
+        });
+    },
+
+    model: function() {
+        return this.getDevices();
+    },
+
+    getDevices: function() {
+        var devices = this.get('guardData').getDevices();
+        var _d = [];
+        for (var i = 0; i < devices.length; i++) {
+            var device = devices[i];
+            var status = "device-ok";
+            if (device.status != "ok")
+                status = "device-error"
+            var guardCount = 0;
+            var alarmCount = 0;
+            for (var j = 0; j < device.guards.length; j++) {
+                var guard = device.guards[j];
+                guardCount++;
+                alarmCount += guard.alarms.length;
+            }
+            _d.push(
+                {
+                    name: device.name,
+                    guardCount: guardCount,
+                    alarmCount: alarmCount,
+                    status: status,
+                });
+        }
+        return _d;
+    },
 });
